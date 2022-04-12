d23b746fde6bd2cd69ad34c1cdfb7ff52630dfb8
Behzad-Az <behy1368@gmail.com>
2022-04-12 10:15:00 -0700

    created blank components

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
 import { About, Footer, Header, Skills, Testimonials, Work } from './containers';
+import { Navbar } from './components';
 
 interface Props {};
 
 const App: FC<Props> = () : JSX.Element => {
   return (
     <div className='app'>
+      <Navbar />
       <Header />
       <About />
       <Work />
---
+++ frontend-react/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './Navbar.scss';
+
+interface Props {};
+
+const Navbar: FC<Props> = () : JSX.Element => {
+  return (
+    <div>Navbar</div>
+  );
+};
+
+export default Navbar;
