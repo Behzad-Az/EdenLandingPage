23ad94e315521d8a213e14d0d3ca9c8de3c059e3
Behzad-Az <behy1368@gmail.com>
2022-04-12 12:19:32 -0700

    working on header

--- frontend-react/src/containers/Header/Header.tsx
+++ frontend-react/src/containers/Header/Header.tsx
@@ -1,11 +1,76 @@
 import React, { FC } from 'react';
+import { motion } from 'framer-motion';
+
+import { images } from '../../constants';
 import './Header.scss';
 
 interface Props {};
 
 const Header: FC<Props> = () : JSX.Element => {
+
+  const scaleVariants = {
+    whileInView: {
+      scale: [0, 1],
+      opacity: [0, 1],
+      transition: {
+        duration: 1,
+        ease: 'easeInOut'
+      }
+    }
+  };
+
   return (
-    <div>Header</div>
+    <div className='app__header app__flex'>
+
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className='app__header-info'
+      >
+        <div className='app__header-badge'>
+
+          <div className='badge-cmp app__flex'>
+            <span>👋🏼</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className='p-text'>Hello, I am</p>
+              <h1 className='head-text'>Behzad</h1>
+            </div>
+          </div>
+
+          <div className='tag-cmp app__flex'>
+            <p className='p-text'>Web Developer</p>
+            <p className='p-text'>Freelancer</p>
+          </div>
+
+        </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className='app__header-img'
+      >
+        <img src={images.profile} alt='profile_bg' />
+        <motion.svg
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          xmlns={images.circle}
+          width={300}
+          height={300}
+          // alt='profile_circle'
+          className='overlay_circle'
+        />
+      </motion.div>
+
+      <motion.div
+        variants={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className='app__header-circles'
+      >
+        
+      </motion.div>
+
+    </div>
   );
 };
 
